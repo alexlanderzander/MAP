8fbb567d399dc50a36e646c43081eb9516efcfaf
alexlanderzander <162145428+alexlanderzander@users.noreply.github.com>
2026-08-22 19:29:16 +0200

    test: add C# decoder stubs

---
+++ tools/csharp_smoke/UnityEngineStubs.cs
@@ -0,0 +1,9 @@
+namespace UnityEngine
+{
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+        public Vector2(float x, float y) { this.x = x; this.y = y; }
+    }
+}
